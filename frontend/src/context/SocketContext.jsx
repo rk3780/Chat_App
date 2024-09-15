ad489a5a8660533b9a71d0524b9b3a23154f4584
rk3780 <rishikhandelwal3780@gmail.com>
2024-09-15 13:38:47 +0530

    third commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chat-app-k84t.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
